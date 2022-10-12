b18c25fb95406c2bbb97a07120b4553dc74e41ad
Hyeon-Jun-Kim <59905688+Hyeon-Jun-Kim@users.noreply.github.com>
2022-10-12 17:36:38 +0900

    [ Login & Sign up UI ] - Getting Started With Login UI

---
+++ TwitterClone/Contoller/Authentication/LoginController.swift
@@ -0,0 +1,41 @@
+//
+//  LoginController.swift
+//  TwitterClone
+//
+//  Created by 김현준 on 2022/10/12.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    
+    // MARK: - Properties
+    private let logoImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        iv.image = UIImage(named: "TwitterLogo")
+        return iv
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: - ASelectors
+    
+    // MARK: - Helpers
+    func configureUI() {
+        view.backgroundColor = .twitterBlue
+        navigationController?.navigationBar.barStyle = .black
+        navigationController?.navigationBar.isHidden = true
+        
+        view.addSubview(logoImageView)
+        logoImageView .centerX(inView: view, topAnchor: view.safeAreaLayoutGuide.topAnchor)
+        logoImageView.setDimensions(width: 150, height: 150)
+    }
+    
+}
