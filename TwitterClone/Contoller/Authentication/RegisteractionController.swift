cd131f3c33d9fe04d7f050731a1e5baf0e613721
Hyeon-Jun-Kim <59905688+Hyeon-Jun-Kim@users.noreply.github.com>
2022-10-13 10:33:48 +0900

    [ Login & Signup UI ] - Creating Input Container View

---
+++ TwitterClone/Contoller/Authentication/RegisteractionController.swift
@@ -0,0 +1,28 @@
+//
+//  RegisteractionController.swift
+//  TwitterClone
+//
+//  Created by 김현준 on 2022/10/12.
+//
+
+import UIKit
+
+class RegisteractionController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .twitterBlue
+    }
+    
+    // MARK: - ASelectors
+    
+    // MARK: - Helpers
+    func configureUI() {
+        
+    }
+    
+}
