7ce183af29139c85d58865134af73e4dac5a45bc
Hyeon-Jun-Kim <59905688+Hyeon-Jun-Kim@users.noreply.github.com>
2022-10-18 23:47:26 +0900

    [ Login & Signup UI ] - Authenticate User

--- TwitterClone/Contoller/Authentication/RegisteractionController.swift
+++ TwitterClone/Contoller/Authentication/RegisteractionController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisteractionController: UIViewController{
     
@@ -99,7 +100,17 @@ class RegisteractionController: UIViewController{
     }
     
     @objc func handleRegisteration() {
+        guard let email = emailTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
         
+        Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
+            if let error = error {
+                print("DEBUG : Error is \(error.localizedDescription)")
+                return
+            }
+            
+            
+        }
     }
     
     @objc func handleShowLogin() {
