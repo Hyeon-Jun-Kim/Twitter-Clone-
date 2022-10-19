c561d33ab1c611e2dcf45b29b8a938208f4087de
Hyeon-Jun-Kim <59905688+Hyeon-Jun-Kim@users.noreply.github.com>
2022-10-19 23:35:41 +0900

    [ Login & Signup ] - Upload User Profile Image

---
+++ TwitterClone/Utils/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  TwitterClone
+//
+//  Created by 김현준 on 2022/10/19.
+//
+
+import Firebase
+
+let STORAGE_REF = Storage.storage().reference()
+let STORAGE_PROFILE_IMAGES = STORAGE_REF.child("profile_images")
+
+let DB_REF = Database.database().reference()
+let REF_USERS = DB_REF.child("users")
