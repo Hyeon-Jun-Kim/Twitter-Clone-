ba18164a7a6f1f5e21697a6b8e9e99a168212216
Hyeon-Jun-Kim <59905688+Hyeon-Jun-Kim@users.noreply.github.com>
2022-10-21 15:06:45 +0900

    [ Login & Signout UI ] - Root ViewController Setup & Keeping User Logged In

--- TwitterClone/Contoller/MainTabController.swift
+++ TwitterClone/Contoller/MainTabController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class MainTabController: UITabBarController {
 
@@ -23,15 +24,36 @@ class MainTabController: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        configeureViewControllers()
-        configureUI()
+        
+        view.backgroundColor = .twitterBlue
+        authenticateUserAndConfugureUI()
+    }
+    
+    // MARK: API
+    func authenticateUserAndConfugureUI() {
+        if Auth.auth().currentUser == nil { // User is Not loggen in
+            DispatchQueue.main.async {
+                let nav = UINavigationController(rootViewController: LoginController())
+                nav.modalPresentationStyle = .fullScreen
+                self.present(nav, animated: true, completion: nil)
+            }
+        } else { // User is loggen in
+            configeureViewControllers()
+            configureUI()
+        }
+    }
+    
+    func logUserOut() {
+        do {
+            try Auth.auth().signOut()
+        } catch let error {
+            print("DEBUG: Failed to Sign out with error: \(error.localizedDescription)")
+        }
     }
     
     // MARK: - Selectors
     @objc
     func actionButtonTapped() {
-        
     }
     
     // MARK: - Helpers
