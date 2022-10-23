c706c36bc1973c6910223851fd7dcf3dff985390
Hyeon-Jun-Kim <59905688+Hyeon-Jun-Kim@users.noreply.github.com>
2022-10-24 00:55:24 +0900

    [ User Profile ] - Profile Header Filter Bar

---
+++ TwitterClone/View/ProfileFilterCell.swift
@@ -0,0 +1,42 @@
+//
+//  ProfileFilterCell.swift
+//  TwitterClone
+//
+//  Created by 김현준 on 2022/10/24.
+//
+
+import Foundation
+import UIKit
+
+class ProfileFilterCell: UICollectionViewCell {
+    // MARK: - Properties
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .lightGray
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.text = "test filter"
+        return label
+    }()
+    
+    override var isSelected: Bool {
+        didSet {
+            titleLabel.font = isSelected ? UIFont.boldSystemFont(ofSize: 16) : UIFont.systemFont(ofSize: 14)
+            titleLabel.textColor = isSelected ? .twitterBlue : .lightGray
+        }
+    }
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .white
+        
+        addSubview(titleLabel)
+        titleLabel.center(inView: self)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
