20a727e683c8d97f56580f616d55ac3a8386965e
Hyeon-Jun-Kim <59905688+Hyeon-Jun-Kim@users.noreply.github.com>
2022-10-23 14:01:48 +0900

    [ Loading Tweets In Feed ] - Fetch Tweets From Firebase & Create Tweet Object

---
+++ TwitterClone/Model/Tweet.swift
@@ -0,0 +1,32 @@
+//
+//  Tweet.swift
+//  TwitterClone
+//
+//  Created by 김현준 on 2022/10/23.
+//
+
+import Foundation
+
+struct Tweet {
+    let caption: String
+    let tweetID: String
+    let uid: String
+    let likes: Int
+    var timestamp: Date!
+    let reweetCount: Int
+    
+    init(tweetID: String, dictionary: [String: Any]) {
+        self.tweetID = tweetID
+        
+        self.caption = dictionary["caption"] as? String ?? ""
+        self.uid = dictionary["uid"] as? String ?? ""
+        self.likes = dictionary["likes"] as? Int ?? 0
+        self.reweetCount = dictionary["retweets"] as? Int ?? 0
+        
+        if let timestamp = dictionary["timestamp"] as? Double {
+            self.timestamp = Date(timeIntervalSince1970: timestamp)
+        }
+        
+        
+    }
+}
