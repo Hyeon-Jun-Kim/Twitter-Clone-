d025b2bea8a302374bde219b1fafd7d804d13dd9
Hyeon-Jun-Kim <59905688+Hyeon-Jun-Kim@users.noreply.github.com>
2022-10-26 23:08:57 +0900

    [ Custom Action Sheet ] - Animation Table View / Fade View

--- TwitterClone/Utils/ActionSheetLauncher.swift
+++ TwitterClone/Utils/ActionSheetLauncher.swift
@@ -17,12 +17,32 @@ class ActionSheetLauncher: NSObject {
     private let tableview = UITableView()
     private var window: UIWindow?
     
+    private lazy var blackView: UIView = {
+        let view = UIView()
+        view.alpha = 0
+        view.backgroundColor = UIColor(white: 0, alpha: 0.5)
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleDismissal))
+        view.addGestureRecognizer(tap)
+        
+        return view
+    }()
+    
     // MARK: - Lifecycle
     
     init(user: User){
         self.user = user
         super.init()
         configureTableView()
+    }
+    
+    // MARK: - Selectors
+    
+    @objc func handleDismissal() {
+        UIView.animate(withDuration: 0.5) {
+            self.blackView.alpha = 0
+            self.tableview.frame.origin.y += 300
+        }
     }
     
     // MARK: - Helpers
@@ -33,9 +53,17 @@ class ActionSheetLauncher: NSObject {
         guard let window = UIApplication.shared.windows.first(where: { $0.isKeyWindow }) else { return }
         self.window = window
         
+        window.addSubview(blackView)
+        blackView.frame = window.frame
+        
         window.addSubview(tableview)
-        tableview.frame = CGRect(x: 0, y: window.frame.height - 300,
+        tableview.frame = CGRect(x: 0, y: window.frame.height,
                                  width: window.frame.width, height: 300)
+        
+        UIView.animate(withDuration: 0.5) {
+            self.blackView.alpha = 1
+            self.tableview.frame.origin.y -= 300
+        }
     }
     
     func configureTableView() {
