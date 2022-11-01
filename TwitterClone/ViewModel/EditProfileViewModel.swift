8bbe6b00b62ae5f5b36c0f8ecf4c3ae2eebfd67a
Hyeon-Jun-Kim <59905688+Hyeon-Jun-Kim@users.noreply.github.com>
2022-11-02 03:49:18 +0900

    [ Edit Profile ] - Missing Lecture : Populating Cells With View Model

--- TwitterClone/ViewModel/EditProfileViewModel.swift
+++ TwitterClone/ViewModel/EditProfileViewModel.swift
@@ -17,9 +17,38 @@ enum EditProfileOptions: Int, CaseIterable {
         switch self {
         case .username : return "Username"
         case .fullname : return "Name"
-        case .bio : return "bio"
+        case .bio : return "Bio"
+        }
+    }
+}
+
+struct EditProfileViewModel {
+    
+    private let user: User
+    let option: EditProfileOptions
+    
+    var titleText: String {
+        return option.description
+    }
+    
+    var optionValue: String? {
+        switch option {
+        case .fullname: return user.fullname
+        case .username: return user.username
+        case .bio: return user.bio
         }
     }
     
+    var shouldHideTextField: Bool {
+        return option == .bio
+    }
     
+    var shouldHideTextView: Bool {
+        return option != .bio
+    }
+    
+    init(user: User, option: EditProfileOptions) {
+        self.user = user
+        self.option = option
+    }
 }
